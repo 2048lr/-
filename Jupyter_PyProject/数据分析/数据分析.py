48e241792f7636cd6c2a43de0aa6adb28971b10d
2048lr <105421754+2048lr@users.noreply.github.com>
2024-12-20 22:09:27 +0800

    Delete Jupyter_PyProject/数据分析 directory

--- Jupyter_PyProject/数据分析/数据分析.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-stock_change=np.random.normal(0,1,(10,5))
-print(stock_change)
-pd.DataFrame(data=stock_change,index=stock)
-stock=['股票{}'.format(i) for i in range(10)]
-pd.date_range(start='20201001',end='20200106')
